2d7c8b1a77d87b9706570cc8891b2402a3da0d4d
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2019-07-19 10:40:13 -0300

    including photos

--- src/views/UserEdit.vue
+++ src/views/UserEdit.vue
@@ -1,25 +1,26 @@
 <template>
   <div>
+    <UserPhoto />
     <div class="columns is-mobile is-multiline">
       <div class="column is-12">
         <form @submit.prevent="update()">
           <b-field label="Nome">
-            <b-input v-model="account.name" />
-          </b-field>
-
+            <b-input v-model="account.name" />  
+          </b-field>    
+        
           <b-field label="Educação">
-            <b-input v-model="account.college" />
+            <b-input v-model="account.college" />  
           </b-field>
 
           <b-field label="Empresa">
-            <b-input v-model="account.company" />
+            <b-input v-model="account.company" />  
           </b-field>
 
           <b-field label="Descrição">
-            <b-input type="textarea" v-model="account.description" />
+            <b-input type="textarea" v-model="account.description" />  
           </b-field>
 
-          <input type="submit" class="button is-fullwidth has-text-white" value="Atualizar" />
+          <input type="submit" class="button is-fullwidth has-text-white" value="Atualizar">
         </form>
       </div>
     </div>
@@ -27,44 +28,45 @@
 </template>
 
 <style lang="scss" scoped>
-div.columns {
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
+  div.columns {
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
 
-input[type="submit"] {
-  background-color: #ff6666;
-}
+  input[type="submit"] {
+    background-color: #FF6666;
+  }
 </style>
 
 
 <script>
-import { mapState } from "vuex";
-import store from "../store";
-import router from "../router";
+  import { mapState } from 'vuex';
+  import store from '../store';
+  import router from '../router';
+  import UserPhoto from '../components/UserPhoto';
+  
+  export default {
+    components: {
+      UserPhoto
+    },
 
-export default {
-  computed: {
-    ...mapState({
-      account: state => state.Account.account
-    })
-  },
+    computed: {
+      ...mapState({
+        account: state => state.Account.account
+      })
+    },
 
-  watch: {
-    account() {
-      router.push("/profile");
-    }
-  },
+    watch: {
+      account(newValue, oldValue) {
+        router.push("/profile");
+      }
+    },
 
-  methods: {
-    update() {
-      store.dispatch("updateAccount", {
-        name: this.account.name,
-        college: this.account.college,
-        company: this.account.company,
-        description: this.account.description
-      });
+    methods: {
+      update() {
+        store.dispatch("updateAccount", { name: this.account.name, college: this.account.college, 
+                                          company: this.account.company, description: this.account.description })
+      }
     }
   }
-};
 </script>
