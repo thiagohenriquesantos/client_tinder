94c7d38129528531383e61a203866de80fbe645c
thiagohenriquesantos <thiagosantosengcivil@gmail.com>
2019-07-19 10:31:29 -0300

    like and dislike included

--- src/services/match_service.js
+++ src/services/match_service.js
@@ -7,5 +7,15 @@ export default {
       headers: store.getters["accountHeaders"]
     });
     return response.data;
+  },
+  async like(user_id, liked) {
+    let response = await axios.post(
+      `users/${user_id}/like`,
+      { like: { liked: liked } },
+      {
+        headers: store.getters["accountHeaders"]
+      }
+    );
+    return response.data;
   }
 };
